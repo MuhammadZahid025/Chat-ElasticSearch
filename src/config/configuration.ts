f8b21c106ca19f1a20d35627db2c9e99c15ff3d0
kwanso-zahid <mzahid14cs25@gmail.com>
2023-01-16 16:51:22 +0500

    changeEnv

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -20,7 +20,7 @@ export default () => ({
   database: {
     type: process.env.DATABASE_TYPE || 'postgres',
     host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.PORT) || 4000,
+    port: parseInt(process.env.DATABASE_PORT) || 5432,
     username: process.env.DATABASE_USERNAME || 'postgres',
     password: process.env.DATABASE_PASSWORD || 'password',
     udatabase: process.env.DATABASE_NAME || 'nest-assignment',
